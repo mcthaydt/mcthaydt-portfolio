59c10dd15de0abf6549b28902bf97196a290c3bd
Dawson M <107786093+mcthaydt@users.noreply.github.com>
2024-07-01 12:05:12 -0500

    recruiter friendly

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,18 @@ interface IConfig {
     stack: string;
     hobby: string;
     projectLink: string;
+    businessLink: string;
   };
   socials: {
     [name: string]: string;
   };
   projects: {
+    [name: string]: {
+      url: string;
+      tags: string[];
+    };
+  };
+  companies: {
     [name: string]: {
       url: string;
       tags: string[];
@@ -26,21 +33,32 @@ interface IConfig {
 export const Config: IConfig = {
   me: {
     name: "Dawson McThay",
-    job: "Gen-Z Entrepreneur and Account Manager at Coast Incorporated",
-    started: "2024-06-01",
-    stack: "Verizon Wireless",
+    job: "Gen-Z Entrepreneur",
+    started: "2022-12-11",
+    stack: "Erikson Institute",
     hobby: "gamble my life savings on meme stocks",
-    projectLink: "https://mcthaydt.netlify.app",
+    projectLink: "https://tropicalsos.itch.io/abyss-of-the-self",
+    businessLink: "https://linktr.ee/tropicalsos",
   },
   socials: {
-    linktree: "https://linktr.ee/mcthaydt",
-    twitter: "https://twitter.com/mcthaydt",
+    website: "https://mcthaydt.netlify.app",
     linkedin: "https://www.linkedin.com/in/dawsonmcthay/",
+    twitter: "https://twitter.com/mcthaydt",
   },
   projects: {
     "Abyss of The Self": {
       url: "https://tropicalsos.itch.io/abyss-of-the-self",
       tags: ["< 10 Copies Sold", "< $100 Profit"],
+    },
+  },
+  companies: {
+    "TropicalSOS Accelerator": {
+      url: "https://linktr.ee/tropicalsos",
+      tags: ["President", "Founder"],
+    },
+    "717 Interactive": {
+      url: "https://717interactive.netlify.app",
+      tags: ["President", "Founder"],
     },
   },
   og: {
